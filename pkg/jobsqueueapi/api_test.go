065cf80f8b103329718c5f8a5687c154e490eabd
Jakub Matějka <jakub@keboola.com>
2022-07-26 17:40:18 +0200

    Support Jobs Queue API

---
+++ pkg/jobsqueueapi/api_test.go
@@ -0,0 +1,83 @@
+package jobsqueueapi_test
+
+import (
+	"context"
+	"github.com/keboola/go-client/pkg/client"
+	"github.com/keboola/go-client/pkg/jobsqueueapi"
+	"github.com/keboola/go-client/pkg/storageapi"
+	"github.com/keboola/go-utils/pkg/orderedmap"
+	"github.com/keboola/go-utils/pkg/testproject"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestJobsQueueApiCalls(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	_, storageClient, jobsQueueClient := clientsForAnEmptyProject(t)
+
+	// Get default branch
+	branch, err := storageapi.GetDefaultBranchRequest().Send(ctx, storageClient)
+	assert.NoError(t, err)
+	assert.NotNil(t, branch)
+
+	// List - no component/config
+	components, err := storageapi.ListConfigsAndRowsFrom(branch.BranchKey).Send(ctx, storageClient)
+	assert.NoError(t, err)
+	assert.Empty(t, components)
+
+	// Create config
+	config := &storageapi.ConfigWithRows{
+		Config: &storageapi.Config{
+			ConfigKey: storageapi.ConfigKey{
+				BranchID:    branch.ID,
+				ComponentID: "ex-generic-v2",
+			},
+			Name:              "Test",
+			Description:       "Test description",
+			ChangeDescription: "My test",
+			Content: orderedmap.FromPairs([]orderedmap.Pair{
+				{Key: "foo", Value: "bar"},
+			}),
+		},
+		Rows: []*storageapi.ConfigRow{},
+	}
+	resConfig, err := storageapi.CreateConfigRequest(config).Send(ctx, storageClient)
+	assert.NoError(t, err)
+	assert.Same(t, config, resConfig)
+	assert.NotEmpty(t, config.ID)
+
+	// Run a job on the config
+	resJob, err := jobsqueueapi.CreateJobRequest("ex-generic-v2", config.ID).Send(ctx, jobsQueueClient)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, resJob.ID)
+
+	// Wait for the job
+	err = jobsqueueapi.WaitForJob(ctx, jobsQueueClient, resJob)
+	// The job payload is malformed, so it fails. We are checking just that it finished.
+	assert.ErrorContains(t, err, "Unrecognized option \"foo\" under \"container\"")
+}
+
+func clientsForAnEmptyProject(t *testing.T) (*testproject.Project, client.Sender, client.Sender) {
+	ctx := context.Background()
+	project := testproject.GetTestProject(t)
+
+	// Get Storage API client
+	storageApiClient := storageapi.ClientWithHostAndToken(client.NewTestClient(), project.StorageAPIHost(), project.StorageAPIToken())
+
+	// Clean project
+	if _, err := storageapi.CleanProjectRequest().Send(ctx, storageApiClient); err != nil {
+		t.Fatalf(`cannot clean project "%d": %s`, project.ID(), err)
+	}
+
+	// Get Scheduler API host
+	index, err := storageapi.IndexRequest().Send(ctx, storageApiClient)
+	assert.NoError(t, err)
+	jobsQueueHost, found := index.AllServices().URLByID("queue")
+	assert.True(t, found)
+
+	// Get jobs queue client
+	jobsQueueApiClient := jobsqueueapi.ClientWithHostAndToken(client.NewTestClient(), jobsQueueHost.String(), project.StorageAPIToken())
+
+	return project, storageApiClient, jobsQueueApiClient
+}
---
+++ pkg/jobsqueueapi/job.go
@@ -0,0 +1,48 @@
+package jobsqueueapi
+
+import (
+	jsonLib "encoding/json"
+)
+
+// JobID is an ID of a component job.
+type JobID string
+
+func (j JobID) String() string {
+	return string(j)
+}
+
+// JobKey is a unique identifier of Job.
+type JobKey struct {
+	ID JobID `json:"id"`
+}
+
+type JobResult struct {
+	Error   map[string]any `json:"error,omitempty"`
+	Message string         `json:"message,omitempty"`
+}
+
+// UnmarshalJSON implements JSON decoding.
+func (r *JobResult) UnmarshalJSON(data []byte) (err error) {
+	if string(data) == "[]" {
+		*r = JobResult{
+			Error:   nil,
+			Message: "",
+		}
+		return nil
+	}
+	// see https://stackoverflow.com/questions/43176625/call-json-unmarshal-inside-unmarshaljson-function-without-causing-stack-overflow
+	type _r JobResult
+	return jsonLib.Unmarshal(data, (*_r)(r))
+}
+
+// Job is a component job.
+type Job struct {
+	JobKey
+	Status     string    `json:"status"`
+	IsFinished bool      `json:"isFinished"`
+	URL        string    `json:"url"`
+	Result     JobResult `json:"result,omitempty"`
+	CreateTime Time      `json:"createdTime"`
+	StartTime  *Time     `json:"startTime"`
+	EndTime    *Time     `json:"endTime"`
+}
