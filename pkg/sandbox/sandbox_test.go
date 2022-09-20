06375816f01fac1e417b64f9952e1985005b77ac
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-09-20 11:57:00 +0200

    remove comment

--- pkg/sandbox/sandbox_test.go
+++ pkg/sandbox/sandbox_test.go
@@ -60,48 +60,6 @@ func TestCreateAndDeleteSandbox(t *testing.T) {
 		)
 		assert.NoError(t, err)
 	}
-
-	// Create sandbox
-	/* {
-		// Create sandbox config (so UI can see it)
-		sandboxConfig, err := sandbox.CreateConfigRequest(branch.ID, "test").Send(ctx, sapiClient)
-		assert.NoError(t, err)
-		assert.NotNil(t, sandboxConfig)
-
-		// Create sandbox from config
-		_, err = sandbox.CreateJobRequest(
-			sandboxConfig.ID,
-			"python",
-			sandbox.WithExpireAfterHours(1),
-			sandbox.WithSize(sandbox.SizeMedium),
-		).Send(ctx, queueClient)
-		assert.NoError(t, err)
-
-		// Get sandbox config
-		// The initial config does not have the sandbox id, because the sandbox has not been created yet,
-		// so we need to fetch the sandbox config after the sandbox create job finishes.
-		// The sandbox id is separate from the sandbox config id, and we need both to delete the sandbox.
-		config, err := sandbox.GetConfigRequest(branch.ID, sandboxConfig.ID).Send(ctx, sapiClient)
-		assert.NoError(t, err)
-		assert.NotNil(t, config)
-
-		configId = config.ID
-		idParam, found, err := config.Content.GetNested("parameters.id")
-		assert.NoError(t, err)
-		assert.True(t, found, "configuration is missing parameters.id")
-		sandboxId = sandbox.SandboxID(idParam.(string))
-	} */
-
-	// Delete sandbox
-	/* {
-		// Delete sandbox (this stops the instance and deletes it)
-		_, err := sandbox.DeleteJobRequest(configId, sandboxId).Send(ctx, queueClient)
-		assert.NoError(t, err)
-
-		// Delete sandbox config (so it is no longer visible in UI)
-		_, err = sandbox.DeleteConfigRequest(branch.ID, configId).Send(ctx, sapiClient)
-		assert.NoError(t, err)
-	} */
 }
 
 func clientsForAnEmptyProject(t *testing.T) (*testproject.Project, client.Sender, client.Sender) {
