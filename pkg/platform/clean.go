d6b659dc1a5d221d3f68c83bd6ef54bbaa3b1dcc
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-10-13 22:14:15 +0200

    Run `CleanProject` requests in parallel

--- pkg/platform/clean.go
+++ pkg/platform/clean.go
@@ -2,7 +2,9 @@ package platform
 
 import (
 	"context"
+	"sync"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/keboola/go-client/pkg/client"
 	"github.com/keboola/go-client/pkg/sandboxesapi"
 	"github.com/keboola/go-client/pkg/schedulerapi"
@@ -15,15 +17,41 @@ func CleanProject(
 	schedulerClient client.Sender,
 	sandboxClient client.Sender,
 ) error {
-	if err := storageapi.CleanProjectRequest().SendOrErr(ctx, storageClient); err != nil {
-		return err
-	}
+	wg := &sync.WaitGroup{}
+	errors := make(chan error)
 
-	if err := schedulerapi.CleanAllSchedulesRequest().SendOrErr(ctx, schedulerClient); err != nil {
-		return err
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := storageapi.CleanProjectRequest().SendOrErr(ctx, storageClient); err != nil {
+			errors <- err
+		}
+	}()
 
-	if err := sandboxesapi.CleanInstancesRequest().SendOrErr(ctx, sandboxClient); err != nil {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := schedulerapi.CleanAllSchedulesRequest().SendOrErr(ctx, schedulerClient); err != nil {
+			errors <- err
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := sandboxesapi.CleanInstancesRequest().SendOrErr(ctx, sandboxClient); err != nil {
+			errors <- err
+		}
+	}()
+
+	wg.Wait()
+	close(errors)
+
+	var err error
+	for e := range errors {
+		err = multierror.Append(err, e)
+	}
+	if err != nil {
 		return err
 	}
 
