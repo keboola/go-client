12e4c96662850ca58118704d17e3e61e9aec0dc8
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-10-13 22:14:39 +0200

    Refactor `clean_test.go` dependencies into separate function

--- pkg/platform/clean_test.go
+++ pkg/platform/clean_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/keboola/go-client/pkg/client"
+	"github.com/keboola/go-client/pkg/jobsqueueapi"
 	"github.com/keboola/go-client/pkg/platform"
 	"github.com/keboola/go-client/pkg/sandboxesapi"
 	"github.com/keboola/go-client/pkg/schedulerapi"
@@ -16,62 +17,83 @@ import (
 func TestCleanProject(t *testing.T) {
 	t.Parallel()
 
-	ctx := context.Background()
-	project := testproject.GetTestProject(t)
-
-	// Get Storage API client
-	storageClient := storageapi.ClientWithHostAndToken(client.NewTestClient(), project.StorageAPIHost(), project.StorageAPIToken())
-
-	// Get API hosts
-	index, err := storageapi.IndexRequest().Send(ctx, storageClient)
-	assert.NoError(t, err)
-	services := index.AllServices()
-
-	// Get Sandbox client
-	sandboxHost, found := services.URLByID("sandboxes")
-	assert.True(t, found)
-	sandboxClient := sandboxesapi.ClientWithHostAndToken(client.NewTestClient(), sandboxHost.String(), project.StorageAPIToken())
-
-	// Get Scheduler client
-	schedulerHost, found := services.URLByID("scheduler")
-	assert.True(t, found)
-	schedulerClient := schedulerapi.ClientWithHostAndToken(client.NewTestClient(), schedulerHost.String(), project.StorageAPIToken())
+	ctx, project, c := deps(t)
 
 	// Clean project
-	if err := platform.CleanProject(ctx, storageClient, schedulerClient, sandboxClient); err != nil {
+	if err := platform.CleanProject(ctx, c.StorageClient, c.ScheduleClient, c.SandboxClient); err != nil {
 		t.Fatalf(`cannot clean project "%d": %s`, project.ID(), err)
 	}
 
 	// Assert that project is clean
 
 	// Only default branch exists
-	branches, err := storageapi.ListBranchesRequest().Send(ctx, storageClient)
+	branches, err := storageapi.ListBranchesRequest().Send(ctx, c.StorageClient)
 	assert.NoError(t, err)
 	assert.Len(t, *branches, 1)
 	defaultBranch := (*branches)[0].BranchKey
 
 	// Default branch has no metadata
-	metadata, err := storageapi.ListBranchMetadataRequest(defaultBranch).Send(ctx, storageClient)
+	metadata, err := storageapi.ListBranchMetadataRequest(defaultBranch).Send(ctx, c.StorageClient)
 	assert.NoError(t, err)
 	assert.Len(t, *metadata, 0)
 
 	// No configs - implies no rows or config metadata
-	configs, err := storageapi.ListConfigsAndRowsFrom(defaultBranch).Send(ctx, storageClient)
+	configs, err := storageapi.ListConfigsAndRowsFrom(defaultBranch).Send(ctx, c.StorageClient)
 	assert.NoError(t, err)
 	assert.Len(t, *configs, 0)
 
 	// No buckets - implies no tables
-	buckets, err := storageapi.ListBucketsRequest().Send(ctx, storageClient)
+	buckets, err := storageapi.ListBucketsRequest().Send(ctx, c.StorageClient)
 	assert.NoError(t, err)
 	assert.Len(t, *buckets, 0)
 
 	// No schedules
-	schedules, err := schedulerapi.ListSchedulesRequest().Send(ctx, schedulerClient)
+	schedules, err := schedulerapi.ListSchedulesRequest().Send(ctx, c.ScheduleClient)
 	assert.NoError(t, err)
 	assert.Len(t, *schedules, 0)
 
 	// No sandbox instances
-	instances, err := sandboxesapi.ListInstancesRequest().Send(ctx, sandboxClient)
+	instances, err := sandboxesapi.ListInstancesRequest().Send(ctx, c.SandboxClient)
 	assert.NoError(t, err)
 	assert.Len(t, *instances, 0)
+}
+
+type testClients struct {
+	StorageClient  client.Sender
+	ScheduleClient client.Sender
+	SandboxClient  client.Sender
+	QueueClient    client.Sender
+}
+
+func deps(t *testing.T) (context.Context, *testproject.Project, *testClients) {
+	t.Helper()
+
+	ctx := context.Background()
+	project := testproject.GetTestProject(t)
+
+	storageClient := storageapi.ClientWithHostAndToken(client.NewTestClient(), project.StorageAPIHost(), project.StorageAPIToken())
+
+	index, err := storageapi.IndexRequest().Send(ctx, storageClient)
+	assert.NoError(t, err)
+
+	services := index.AllServices()
+	schedulerApiHost, found := services.URLByID("scheduler")
+	assert.True(t, found)
+	sandboxesApiHost, found := services.URLByID("sandboxes")
+	assert.True(t, found)
+	jobsQueueHost, found := services.URLByID("queue")
+	assert.True(t, found)
+
+	scheduleClient := schedulerapi.ClientWithHostAndToken(client.NewTestClient(), schedulerApiHost.String(), project.StorageAPIToken())
+	sandboxClient := sandboxesapi.ClientWithHostAndToken(client.NewTestClient(), sandboxesApiHost.String(), project.StorageAPIToken())
+	queueClient := jobsqueueapi.ClientWithHostAndToken(client.NewTestClient(), jobsQueueHost.String(), project.StorageAPIToken())
+
+	clients := &testClients{
+		StorageClient:  storageClient,
+		ScheduleClient: scheduleClient,
+		SandboxClient:  sandboxClient,
+		QueueClient:    queueClient,
+	}
+
+	return ctx, project, clients
 }
