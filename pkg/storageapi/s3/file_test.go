cd76e1e50c2898720fa3f8f9d4210c044d7beba7
Michal Jurečko <michal.jurecko@gmail.com>
2022-11-10 07:57:10 +0100

    Move tests to backend pkgs

--- pkg/storageapi/s3/file_test.go
+++ pkg/storageapi/s3/file_test.go
@@ -3,7 +3,9 @@ package s3_test
 import (
 	"testing"
 
+	"github.com/keboola/go-client/pkg/storageapi/s3"
 	"github.com/keboola/go-utils/pkg/testproject"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/keboola/go-client/pkg/storageapi"
 	"github.com/keboola/go-client/pkg/storageapi/testdata"
@@ -15,4 +17,25 @@ func TestCreateFileResourceAndUpload(t *testing.T) {
 	for _, tc := range testdata.UploadTestCases() {
 		tc.Run(t, storageApiClient)
 	}
+}
+
+func TestCreateImportManifest(t *testing.T) {
+	t.Parallel()
+
+	f := &storageapi.File{
+		Provider: "aws",
+		S3UploadParams: s3.UploadParams{
+			Key:    "exp-15-files-4516-27298008-2022-11-08.test1",
+			Bucket: "kbc-sapi-files",
+		},
+	}
+
+	res, err := storageapi.NewSlicedFileManifest(f, []string{"one", "two"})
+	assert.NoError(t, err)
+
+	e := &storageapi.SlicedFileManifest{Entries: []storageapi.Slice{
+		{Url: "s3://kbc-sapi-files/exp-15-files-4516-27298008-2022-11-08.test1one"},
+		{Url: "s3://kbc-sapi-files/exp-15-files-4516-27298008-2022-11-08.test1two"},
+	}}
+	assert.Equal(t, e, res)
 }
