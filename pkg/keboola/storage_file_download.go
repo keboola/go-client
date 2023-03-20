4a4381abb3af22fb59584661a67389a2cf841ef7
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-03-20 09:07:58 +0100

    add docs about download methods

--- pkg/keboola/storage_file_download.go
+++ pkg/keboola/storage_file_download.go
@@ -28,6 +28,11 @@ func WithDownloadTransport(transport http.RoundTripper) DownloadOption {
 }
 
 // DownloadAll downloads all slices of a file. To download a whole file, use `Download` instead.
+//
+// This downloads each individual slice and concatenates them into a single byte slice.
+// That means this may end up using a lot of memory if your files are large. If you believe
+// this may end up being the case, then use `DownloadManifest` followed by downloading
+// each slice using `DownloadSlice` or `DownloadSliceReader`.
 func DownloadAll(ctx context.Context, file *FileDownloadCredentials) ([]byte, error) {
 	if !file.IsSliced {
 		return nil, fmt.Errorf("cannot download a whole file as a sliced file")
@@ -59,6 +64,9 @@ func Download(ctx context.Context, file *FileDownloadCredentials) ([]byte, error
 	return DownloadSlice(ctx, file, "")
 }
 
+// DownloadManifest downloads the file manifest, which contains a list of slices.
+//
+// This function assumes that `file.IsSliced == true`.
 func DownloadManifest(ctx context.Context, file *FileDownloadCredentials) (SlicesList, error) {
 	rawManifest, err := DownloadSlice(ctx, file, ManifestFileName)
 	if err != nil {
@@ -85,6 +93,11 @@ func DownloadManifest(ctx context.Context, file *FileDownloadCredentials) (Slice
 	return res, nil
 }
 
+// DownloadSlice downloads a single slice and returns it whole as a byte slice.
+//
+// For streaming data, prefer `DownloadSliceReader`.
+//
+// This function assumes that `file.IsSliced == true`.
 func DownloadSlice(ctx context.Context, file *FileDownloadCredentials, slice string) (out []byte, err error) {
 	reader, err := DownloadSliceReader(ctx, file, slice)
 	if err != nil {
@@ -100,14 +113,25 @@ func DownloadSlice(ctx context.Context, file *FileDownloadCredentials, slice str
 	return out, nil
 }
 
+// DownloadReader returns a reader which may be used to download a whole file.
+//
+// This functions assumes that `file.IsSliced == false`.
 func DownloadReader(ctx context.Context, file *FileDownloadCredentials) (io.ReadCloser, error) {
 	return DownloadSliceReader(ctx, file, "")
 }
 
+// DownloadManifestReader returns a reader which may be used to download the manifest of a sliced file.
+//
+// This function assumes that `file.IsSliced == true`.
 func DownloadManifestReader(ctx context.Context, file *FileDownloadCredentials) (io.ReadCloser, error) {
 	return DownloadSliceReader(ctx, file, ManifestFileName)
 }
 
+// DownloadSliceReader returns a reader which may be used to download a single slice of a file.
+//
+// The `DownloadManifest` family of functions may be used to obtain a manifest which contains the list of slices.
+//
+// This function assumes that `file.IsSliced == true`.
 func DownloadSliceReader(ctx context.Context, file *FileDownloadCredentials, slice string, opts ...DownloadOption) (io.ReadCloser, error) {
 	c := downloadConfig{}
 	for _, opt := range opts {
@@ -125,6 +149,9 @@ func DownloadSliceReader(ctx context.Context, file *FileDownloadCredentials, sli
 	}
 }
 
+// GetFileAttributes returns information about a while, such as its content type, last changed time, and byte size.
+//
+// If `file.IsSliced == false`, the `slice` parameter should be an empty string.
 func GetFileAttributes(ctx context.Context, file *FileDownloadCredentials, slice string, opts ...DownloadOption) (*FileAttributes, error) {
 	c := downloadConfig{}
 	for _, opt := range opts {
