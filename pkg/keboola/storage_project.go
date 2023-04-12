d3af2f3848202b7e1e9f4a7c3d7002b1e012f8bf
Jakub Matějka <jakub@keboola.com>
2023-04-12 14:05:22 +0200

    Add String() method to ProjectID

--- pkg/keboola/storage_project.go
+++ pkg/keboola/storage_project.go
@@ -1,3 +1,12 @@
 package keboola
 
+import "fmt"
+
 type ProjectID int
+
+func (v ProjectID) String() string {
+	if v == 0 {
+		panic(fmt.Errorf("projectID cannot be empty"))
+	}
+	return fmt.Sprintf("%d", v)
+}
