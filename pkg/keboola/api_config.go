de6144e1729b3fd078b56dce992efe6ca993c8c4
Michal Jurečko <michal.jurecko@gmail.com>
2023-05-12 07:39:12 +0200

    Add api.WithTracerProvider/WithMeterProvider

--- pkg/keboola/api_config.go
+++ pkg/keboola/api_config.go
@@ -1,11 +1,16 @@
 package keboola
 
 import (
+	otelMetric "go.opentelemetry.io/otel/metric"
+	otelTrace "go.opentelemetry.io/otel/trace"
+
 	"github.com/keboola/go-client/pkg/client"
 )
 
 type apiConfig struct {
 	client         *client.Client
+	tracerProvider otelTrace.TracerProvider
+	meterProvider  otelMetric.MeterProvider
 	token          string
 }
 
@@ -22,6 +27,18 @@ func newAPIConfig(opts []APIOption) apiConfig {
 func WithClient(cl *client.Client) APIOption {
 	return func(c *apiConfig) {
 		c.client = cl
+	}
+}
+
+func WithTracerProvider(v otelTrace.TracerProvider) APIOption {
+	return func(c *apiConfig) {
+		c.tracerProvider = v
+	}
+}
+
+func WithMeterProvider(v otelMetric.MeterProvider) APIOption {
+	return func(c *apiConfig) {
+		c.meterProvider = v
 	}
 }
 
