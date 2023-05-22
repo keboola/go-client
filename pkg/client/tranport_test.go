d998bd304369daab7c04cfa0cb50922a82b69ab0
Michal Jurečko <michal.jurecko@gmail.com>
2023-05-22 07:26:20 +0200

    Update transport definitions

---
+++ pkg/client/tranport_test.go
@@ -0,0 +1,43 @@
+package client_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/keboola/go-client/pkg/client"
+	"github.com/keboola/go-client/pkg/request"
+)
+
+func TestDefaultTransport(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	out := ""
+	url := "https://www.jsontest.com"
+	c := client.New().WithTransport(client.DefaultTransport()) // <<<<<<<<<
+	apiRequest := request.NewAPIRequest(&out, request.NewHTTPRequest(c).WithGet(url).WithResult(&out))
+	result, err := apiRequest.Send(ctx)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, *result)
+
+}
+
+func TestHTTP2Transport(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	out := ""
+	url := "https://www.jsontest.com"
+	c := client.New().WithTransport(client.HTTP2Transport()) // <<<<<<<<<
+	apiRequest := request.NewAPIRequest(&out, request.NewHTTPRequest(c).WithGet(url).WithResult(&out))
+	result, err := apiRequest.Send(ctx)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, *result)
+}
