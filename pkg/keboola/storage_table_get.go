3a269ae90d628b295f13596fadb6ebf29c3f01d7
Michal Jurečko <michal.jurecko@gmail.com>
2023-08-09 08:54:16 +0200

    Split table operations to more files

---
+++ pkg/keboola/storage_table_get.go
@@ -0,0 +1,16 @@
+package keboola
+
+import "github.com/keboola/go-client/pkg/request"
+
+// GetTableRequest https://keboola.docs.apiary.io/#reference/tables/manage-tables/table-detail
+func (a *API) GetTableRequest(k TableKey) request.APIRequest[*Table] {
+	bucketKey := BucketKey{BranchID: k.BranchID, BucketID: k.TableID.BucketID}
+	table := &Table{TableKey: k, Bucket: &Bucket{BucketKey: bucketKey}}
+	req := a.
+		newRequest(StorageAPI).
+		WithResult(table).
+		WithGet("branch/{branchId}/tables/{tableId}").
+		AndPathParam("branchId", k.BranchID.String()).
+		AndPathParam("tableId", k.TableID.String())
+	return request.NewAPIRequest(table, req)
+}
