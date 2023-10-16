e8047d18764a5aed48207f80d9792bded6ff95fe
Michal Jurečko <michal.jurecko@gmail.com>
2023-10-16 16:48:35 +0200

    Update install-tools.sh

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -9,6 +9,26 @@ set -o pipefail         # Use last non-zero exit code in a pipeline
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR"
 
-./install-gotestsum.sh -b $(go env GOPATH)/bin
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@master
-go install golang.org/x/tools/cmd/godoc@latest
+# gotestsum
+if ! command -v gotestsum &> /dev/null
+then
+  ./install-gotestsum.sh -b $(go env GOPATH)/bin
+fi
+
+# golangci-lint
+if ! command -v golangci-lint &> /dev/null
+then
+  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.50.1
+fi
+
+# godoc
+if ! command -v godoc &> /dev/null
+then
+  go install golang.org/x/tools/cmd/godoc@latest
+fi
+
+# go-mod-upgrade
+if ! command -v go-mod-upgrade &> /dev/null
+then
+  go install github.com/oligot/go-mod-upgrade@latest
+fi
